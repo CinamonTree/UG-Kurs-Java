d373d07586e051d0893eb1244de6f2b3071ded64
Rafal <yankovic22@email.com>
2025-12-12 00:20:21 +0100

    Dodano Klasę Price

---
+++ Zadanie programistyczne 1/stock-market-sim/src/main/java/com/stockmarket/domain/Price.java
@@ -0,0 +1,78 @@
+package com.stockmarket.domain;
+
+import java.util.Currency;
+
+public class Price implements Comparable<Price> {
+    
+    private Currency currency;
+    private double amount;
+
+    public Price(Currency currency, double amount) {
+        this.currency = validateCurrency(currency);
+        this.amount = validateAmount(amount);
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = validateCurrency(currency);
+    }
+
+    public void setAmount(double amount) {
+        this.amount = validateAmount(amount);
+    }
+
+    private Currency validateCurrency(Currency currency) {
+        if (currency == null) {
+            throw new IllegalArgumentException("Waluta nie może być nullem.");
+        }
+        return currency;
+    }
+
+    private double validateAmount(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Kwota nie może być ujemna.");
+        }
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency.getCurrencyCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Price other = (Price) obj;
+        return amount == other.amount && currency.equals(other.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        int currencyHash = currency.hashCode(); // hasz z waluty
+        long amountAsLong = Double.doubleToLongBits(amount); // zmiana amount z double na long
+        int amountHash = (int) (amountAsLong ^ (amountAsLong >>> 32)); // hasz z amount
+        int Hash = 31 * currencyHash + amountHash; // kombinacja haszy
+        return Hash;
+    }
+
+    @Override
+    public int compareTo(Price other) {
+        boolean differentCurrencies = !this.currency.equals(other.currency);
+        if (differentCurrencies) {
+            throw new IllegalArgumentException(
+                "Nie można porównać cen w różnych walutach." 
+            );
+        }
+        return Double.compare(this.amount, other.amount);
+    }
+    
+}
---
+++ Zadanie programistyczne 1/stock-market-sim/src/test/java/com/stockmarket/domain/PriceTest.java
@@ -0,0 +1,113 @@
+package com.stockmarket.domain;
+
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PriceTest {
+    
+    private Price USD10;
+    private Price USD20;
+    private Price EUR10;
+    private Currency USD;
+    private Currency EUR;
+
+    @BeforeEach
+    public void setUp() {
+        USD = Currency.getInstance("USD");
+        EUR = Currency.getInstance("EUR");
+        USD10 = new Price(USD, 10.0);
+        USD20 = new Price(USD, 20.0);
+        EUR10 = new Price(EUR, 10.0);
+    }
+        
+    // Konstruktor
+
+    @Test
+    public void shouldCreatePrice() {
+        Price price = new Price(USD, 10.0);
+
+        assertEquals(USD, price.getCurrency());
+        assertEquals(10.0, price.getAmount());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCreatingPriceWithNegativeAmount() {
+        assertThrows(IllegalArgumentException.class,() -> {
+            new Price(USD, -5.0);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCreatingPriceWithNullPointerCurrency() {
+        assertThrows(IllegalArgumentException.class,() -> {
+            new Price(null, 10.0);
+        });
+    }
+
+    // Metoda setCurrency
+
+    @Test
+    public void shouldSetCurrency() {
+        USD10.setCurrency(EUR);
+        assertEquals(EUR, USD10.getCurrency());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSettingCurrencyAsNullPointer() {
+        assertThrows(IllegalArgumentException.class,() -> {
+            USD10.setCurrency(null);
+        });
+    }
+
+    // metoda setAmount
+
+    @Test
+    public void shouldSetAmount() {
+        USD10.setAmount(15.0);
+        assertEquals(15.0, USD10.getAmount());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSettingNegativeAmount() {
+        assertThrows(IllegalArgumentException.class,() -> {
+            USD10.setAmount(-10.0);
+        });
+    }
+
+    // Metoda equals
+
+    @Test
+    public void shouldComparePricesByCurrencyAndAmount() {
+        Price anotherUSD10 = new Price(USD, 10.0);
+        assertEquals(USD10, anotherUSD10);
+    }
+
+    // Metoda hashCode
+
+    @Test
+    public void shouldGenerateHashCodeBasedOnCurrencyAndAmount() {
+        Price anotherUSD10 = new Price(USD, 10.0);
+        assertEquals(USD10.hashCode(), anotherUSD10.hashCode());
+    }
+
+    // Metoda compareTo
+
+    @Test
+    public void shouldComparePricesCorrectly() {
+        assertEquals(-1, USD10.compareTo(USD20));
+        assertEquals(1, USD20.compareTo(USD10));
+        assertEquals(0, USD10.compareTo(new Price(USD, 10.0)));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenComparingPricesWithDiffrentCurrencies() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            USD10.compareTo(EUR10);
+        });
+    }
+
+}
